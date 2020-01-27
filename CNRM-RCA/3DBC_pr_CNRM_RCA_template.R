f280722379a9cdde3d85f9206cafebb70edebc4e
doblerone <andreasd@met.no>
2020-01-27 11:36:38 +0000

    Added CNRM-RCA

---
+++ CNRM-RCA/3DBC_pr_CNRM_RCA_template.R
@@ -0,0 +1,161 @@
+## An implementation of
+## "A Resampling Approach for Correcting Systematic Spatiotemporal Biases for Multiple Variables in a Changing Climate"
+## by Mehrotra and Sharma (2019), Water Resources Research, 55(1), pp.754-770, doi: 10.1029/2018WR023270
+## Refered to as 3DBC (3-dimensional bias-correction), as it keeps inter-variable, temporal and spatial consistencies from the reference data set.
+## Basically a smart combination of quantile-mapping (or any usual bias-correction method) and Schaake Shuffle
+###
+### Implementation by A. Dobler, autumn 2019
+### andreas.dobler@met.no
+
+## This is the template for the R-job chain
+## Generate (6-day) jobs by e.g. (in bash):
+## for ((d=1;d<366;d=d+6)); do let e=d+5; sed "s/START/$d/g; s/END/$e/g" 3DBC_pr_CNRM_RCA_template.R > 3DBC_pr_CNRM_RCA_d${d}.R; done
+
+
+library(ncdf4)
+
+for (d in START:END)
+{
+  daystring <- format(as.Date(d-1, origin = "1980-01-01"),"%m%d") #Format: MMDD
+  
+  #some print output
+  print(paste("Processing day",daystring))
+  print("Reading data")
+  
+  #read data
+  nc <- nc_open(paste("/lustre/storeB/users/andreasd/postclim/3DBC/pr/Obs/seNorge2018_1971-2010_",daystring,".nc",sep=""))
+  ObsA <- ncvar_get(nc,"rr")
+  nc_close(nc)
+  
+  nc <- nc_open(paste("/lustre/storeB/users/andreasd/postclim/3DBC/pr/Cur/hist_CNRM_RCA_RR_daily_1971-2010_",daystring,".nc",sep=""))
+  CurA <- ncvar_get(nc,"precipitation__map_hist_daily")/10
+  
+  #define mask with grid points with values
+  ValMask <- which(!is.na(CurA[,,1]) ,arr.ind=T)
+  NofPoints <- dim(ValMask)[1]
+  
+  #Correct for wrong signs in the input data
+  negval <- which(CurA < 0, arr.ind = T)
+  CurA[negval] <- CurA[negval] + 655.3
+  
+  nc_close(nc)
+  
+  nc <- nc_open(paste("/lustre/storeB/users/andreasd/postclim/3DBC/pr/Fut/rcp85_CNRM_RCA_RR_daily_2011-2100_",daystring,".nc",sep=""))
+  FutA <- ncvar_get(nc,"precipitation__map_rcp85_daily")/10
+  
+  #Correct for wrong signs in the input data
+  negval <- which(FutA < 0, arr.ind = T)
+  FutA[negval] <- FutA[negval] + 655.3
+  
+  nc_close(nc)
+  #reading done
+  
+  #create arrays for corrected data
+  CurCA <- array(NA,dim(CurA))
+  FutCA <- array(NA,dim(FutA))
+  
+  #some print output
+  print("PP started:")
+  print(date())
+  print("------------------------")
+  
+  #loop over grid pointss
+  for (i in 1:NofPoints)  
+  {
+    if ((NofPoints-i) %% 100000 == 0)
+      print(paste(NofPoints-i,"points left to do...",sep=" "))
+    
+    x <- ValMask[i,1]
+    y <- ValMask[i,2]
+    
+    Obs <- ObsA[x,y,]
+    Cur <- CurA[x,y,]
+    FutL <- FutA[x,y,]
+    
+    #number of values
+    nod <- length(Obs)
+    nodf <- length(FutL)
+    nodp1 <- nod+1
+    
+    #Split Fut into blockss
+    futl <- ceiling(nodf/nod) #number of times Obs fits into Fut +1
+    Fut <- array(NA,c(futl,nod))
+    
+    for (s in 1:(futl-1))
+    {
+      Fut[s,] <- as.numeric(unlist(split(FutL, ceiling(seq_along(FutL)/nod))[s]))
+    }            
+    Fut[futl,] <- FutL[(nodf-nod+1):nodf]
+    
+    #Rank data in descending order
+    RankO <- nodp1 -  rank(Obs, ties.method = "first")
+    RankC <- nodp1 -  rank(Cur, ties.method = "first")
+    RankF <- nodp1 -  t(apply(Fut,1,rank,ties.method = "first"))
+    
+    #Cummulative probabilty
+    ProbO <- 1-RankO/nodp1
+    ProbC <- 1-RankC/nodp1
+    ProbF <- 1-RankF/nodp1
+    
+    #Gaussian transformation
+    GaussO <- qnorm(ProbO)
+    GaussC <- qnorm(ProbC)
+    GaussF <- qnorm(ProbF)
+    
+    #Calculate lag-one autocorrelations
+    rO <- acf(GaussO,plot=F,lag.max=1)$acf[2]
+    rC <- acf(GaussC,plot=F,lag.max=1)$acf[2]
+    rF <- apply(GaussF,1,FUN = function (u) c(acf(u, plot = FALSE,lag.max=1)$acf[2]))
+    
+    #Remove rO from GaussO
+    GaussOR <- GaussO/sqrt(1-rO*rO)
+    GaussOR[2:nod] <- (GaussO[2:nod]-rO*GaussO[1:(nod-1)])/sqrt(1-rO*rO) #from 2nd time step
+    
+    #Calculate corrected autocorrelation
+    rN <- ((1+rF) / (1+rC) * (1+rO) - (1-rF) / (1-rC) * (1-rO) ) / ((1+rF) / (1+rC) * (1+rO) + (1-rF) / (1-rC) * (1-rO) ) 
+    
+    #Add rN to GaussOR. Note: Gauss for current climate equals GaussO!
+    GaussFC <- sqrt(1-rN*rN) %*% t(GaussOR)
+    for (t in 2:nod)
+      GaussFC[,t] <- rN*GaussFC[,t-1] + sqrt(1-rN*rN) %*% t(GaussOR[t]) #from 2nd time step
+    
+    ##Rank and reorder values
+    #Current cliamte
+    CurCA[x,y,] <- sort(Cur,decreasing = TRUE)[RankO]
+    
+    #Future climate
+    RankFC <- nodp1 -  t(apply(GaussFC,1,rank,ties.method = "first"))
+    for (s in 1:(futl-1))
+    {
+      FutCA[x,y,(nod*(s-1)+1):(nod*s)] <- sort(Fut[s,],decreasing = TRUE)[RankFC[s,]]
+    }            
+    FutCA[x,y,(nodf-nod+1):nodf] <- sort(Fut[futl,],decreasing = TRUE)[RankFC[futl,]]
+  }
+  #That's all :-)
+  
+  print("------------------------")
+  print("PP finished:")
+  print(date())
+  
+  #Write to NetCDF
+  print("Writing data...")
+  
+  OUT <- CurCA
+  nc <- nc_open(paste("/lustre/storeB/users/andreasd/postclim/3DBC/pr/CurC/hist_CNRM_RCA_RR_daily_1971-2010_",daystring,".nc",sep=""),write=TRUE)
+  ncvar_put(nc,"precipitation__map_hist_daily",OUT)
+  nc_close(nc)
+  
+  OUT <- FutCA
+  nc <- nc_open(paste("/lustre/storeB/users/andreasd/postclim/3DBC/pr/FutC/rcp85_CNRM_RCA_RR_daily_2011-2100_",daystring,".nc",sep=""),write=TRUE)
+  ncvar_put(nc,"precipitation__map_rcp85_daily",OUT)
+  nc_close(nc)
+  
+  print(paste("Day",daystring,"done."))
+  print("===========================")
+  
+  rm(list = ls(all.names = TRUE)) #clear environment
+  gc()                            #free up memrory and report the memory usage.
+  
+}
+
+
--- CNRM-RCA/prepare_data.sh
+++ CNRM-RCA/prepare_data.sh
@@ -1,7 +1,7 @@
 ##########################################################################################################################
 ## Prepare data for 3DBC, i.e. split them into files containing one day of the year each (total 366 files)  ##
 ##########################################################################################################################
-## For CNRM driven CCLM --> Adjust for more models
+## For CNRM driven RCA --> Adjust for more models
 ########################################################
 ## Reference data = seNorge_2018
 ## External data source: http://thredds.met.no/thredds/catalog/metusers/senorge2/seNorge/seNorge_2018/Archive/catalog.html
@@ -9,58 +9,58 @@
 #######################################################
 
 ### Split yearly files into monthly files
-cd /lustre/storeB/project/metkl/senorge2/thredds/seNorge/seNorge_2018/Archive
-for file in seNorge2018_197[1-9].nc seNorge2018_198* seNorge2018_199* seNorge2018_200* seNorge2018_2010.nc
-do cdo splitmon -selvar,tg $file /lustre/storeB/users/andreasd/postclim/3DBC/tas/Obs/${file}_month
-   cdo splitmon -selvar,rr $file /lustre/storeB/users/andreasd/postclim/3DBC/pr/Obs/${file}_month
-done
+#cd /lustre/storeB/project/metkl/senorge2/thredds/seNorge/seNorge_2018/Archive
+#for file in seNorge2018_197[1-9].nc seNorge2018_198* seNorge2018_199* seNorge2018_200* seNorge2018_2010.nc
+#do cdo splitmon -selvar,tg $file /lustre/storeB/users/andreasd/postclim/3DBC/tas/Obs/${file}_month
+#   cdo splitmon -selvar,rr $file /lustre/storeB/users/andreasd/postclim/3DBC/pr/Obs/${file}_month
+#done
 
 ###################
 ## Precipitation
 ###################
 
 # Merge monthly files over all years
-cd /lustre/storeB/users/andreasd/postclim/3DBC/pr/Obs/
-for m in 01 02 03 04 05 06 07 08 09 10 11 12
-do ncrcat seNorge2018_????.nc_month${m}.nc seNorge2018_1971-2010_${m}.nc
-done
+#cd /lustre/storeB/users/andreasd/postclim/3DBC/pr/Obs/
+#for m in 01 02 03 04 05 06 07 08 09 10 11 12
+#do ncrcat seNorge2018_????.nc_month${m}.nc seNorge2018_1971-2010_${m}.nc
+#done
 
 # Remove single monthly files
-rm seNorge2018_????.nc_month??.nc
+#rm seNorge2018_????.nc_month??.nc
 
 # Split into daily files
-for file in seNorge2018_1971-2010_??.nc
-do cdo splitday $file ${file}_day
-done
+#for file in seNorge2018_1971-2010_??.nc
+#do cdo splitday $file ${file}_day
+#done
 
 # Remove merged monthly files
-rm seNorge2018_1971-2010_??.nc
+#rm seNorge2018_1971-2010_??.nc
 
 # Rename files
-rename "s/.nc_day//g" seNorge2018_1971-2010_??.nc_day??.nc
+#rename "s/.nc_day//g" seNorge2018_1971-2010_??.nc_day??.nc
 
 ###################
 ## Temperature
 ###################
 # Merge monthly files over all years
-cd /lustre/storeB/users/andreasd/postclim/3DBC/tas/Obs/
-for m in 01 02 03 04 05 06 07 08 09 10 11 12
-do ncrcat seNorge2018_????.nc_month${m}.nc seNorge2018_1971-2010_${m}.nc
-done
+#cd /lustre/storeB/users/andreasd/postclim/3DBC/tas/Obs/
+#for m in 01 02 03 04 05 06 07 08 09 10 11 12
+#do ncrcat seNorge2018_????.nc_month${m}.nc seNorge2018_1971-2010_${m}.nc
+#done
 
 # Remove single monthly files
-rm seNorge2018_????.nc_month??.nc
+#rm seNorge2018_????.nc_month??.nc
 
 # Split into daily files
-for file in seNorge2018_1971-2010_??.nc
-do cdo splitday $file ${file}_day
-done
+#for file in seNorge2018_1971-2010_??.nc
+#do cdo splitday $file ${file}_day
+#done
 
 # Remove merged monthly files
-rm seNorge2018_1971-2010_??.nc
+#rm seNorge2018_1971-2010_??.nc
 
 # Rename files
-rename "s/.nc_day//g" seNorge2018_1971-2010_??.nc_day??.nc
+#rename "s/.nc_day//g" seNorge2018_1971-2010_??.nc_day??.nc
 
 #######################################################
 ## Climate data = bias-corrected RCM data from KSS/NVE
@@ -73,38 +73,38 @@ rename "s/.nc_day//g" seNorge2018_1971-2010_??.nc_day??.nc
 ###################
 # Split into monthly files
 cd /lustre/storeB/users/andreasd/postclim/3DBC/pr/Cur
-cdo -r -splitmon /lustre/storeB/project/KSS/proj/scenariodata/Precipitation/netcdf/CNRM_CCLM/hist/hist_CNRM_CCLM_RR_daily_1971-2010.nc hist_CNRM_CCLM_RR_daily_1971-2010_month
+cdo -r -splitmon /lustre/storeB/project/KSS/proj/scenariodata/Precipitation/netcdf/CNRM_RCA/hist/hist_CNRM_RCA_RR_daily_1971-2010.nc hist_CNRM_RCA_RR_daily_1971-2010_month
 
 # Split into daily files
-for file in hist_CNRM_CCLM_RR_daily_1971-2010_month??.nc
+for file in hist_CNRM_RCA_RR_daily_1971-2010_month??.nc
 do cdo splitday $file ${file}_day
 done
 
 # Remove monthly files
-rm hist_CNRM_CCLM_RR_daily_1971-2010_month??.nc
+rm hist_CNRM_RCA_RR_daily_1971-2010_month??.nc
 
 # Rename files
-rename "s/.nc_day//g" hist_CNRM_CCLM_RR_daily_1971-2010_month??.nc_day??.nc
-rename "s/month//g" hist_CNRM_CCLM_RR_daily_1971-2010_month????.nc
+rename "s/.nc_day//g" hist_CNRM_RCA_RR_daily_1971-2010_month??.nc_day??.nc
+rename "s/month//g" hist_CNRM_RCA_RR_daily_1971-2010_month????.nc
 
 ###################
 ## Temperature
 ###################
 # Split into monthly files
 cd /lustre/storeB/users/andreasd/postclim/3DBC/tas/Cur
-cdo -r -splitmon /lustre/storeB/project/KSS/proj/scenariodata/Temperature/netcdf/CNRM_CCLM/hist/hist_CNRM_CCLM_TM_daily_1971-2010.nc hist_CNRM_CCLM_TM_daily_1971-2010_month
+cdo -r -splitmon /lustre/storeB/project/KSS/proj/scenariodata/Temperature/netcdf/CNRM_RCA/hist/hist_CNRM_RCA_TM_daily_1971-2010.nc hist_CNRM_RCA_TM_daily_1971-2010_month
 
 # Split into daily files
-for file in hist_CNRM_CCLM_TM_daily_1971-2010_month??.nc
+for file in hist_CNRM_RCA_TM_daily_1971-2010_month??.nc
 do cdo splitday $file ${file}_day
 done
 
 # Remove monthly files
-rm hist_CNRM_CCLM_TM_daily_1971-2010_month??.nc
+rm hist_CNRM_RCA_TM_daily_1971-2010_month??.nc
 
 # Rename files
-rename "s/.nc_day//g" hist_CNRM_CCLM_TM_daily_1971-2010_month??.nc_day??.nc
-rename "s/month//g" hist_CNRM_CCLM_TM_daily_1971-2010_month????.nc
+rename "s/.nc_day//g" hist_CNRM_RCA_TM_daily_1971-2010_month??.nc_day??.nc
+rename "s/month//g" hist_CNRM_RCA_TM_daily_1971-2010_month????.nc
 
 ##########################
 ### Future climate (rcp85)
@@ -112,41 +112,41 @@ rename "s/month//g" hist_CNRM_CCLM_TM_daily_1971-2010_month????.nc
 ## Precipitation
 ###################
 ### Split yearly files into monthly files
-cd /lustre/storeB/project/KSS/proj/scenariodata/Precipitation/netcdf/CNRM_CCLM/rcp85/
-for file in rcp85_CNRM_CCLM_RR_daily_2???_v4.nc
+cd /lustre/storeB/project/KSS/proj/scenariodata/Precipitation/netcdf/CNRM_RCA/rcp85/
+for file in rcp85_CNRM_RCA_RR_daily_2???_v4.nc
 do cdo splitmon $file /lustre/storeB/users/andreasd/postclim/3DBC/pr/Fut/${file}_month
 done
 
 # Merge monthly files over all years
 cd /lustre/storeB/users/andreasd/postclim/3DBC/pr/Fut/
 for m in 01 02 03 04 05 06 07 08 09 10 11 12
-do cdo -r mergetime rcp85_CNRM_CCLM_RR_daily_2???_v4.nc_month${m}.nc rcp85_CNRM_CCLM_RR_daily_2011-2100_${m}.nc
+do cdo -r mergetime rcp85_CNRM_RCA_RR_daily_2???_v4.nc_month${m}.nc rcp85_CNRM_RCA_RR_daily_2011-2100_${m}.nc
 done
 
 # Remove single monthly files
-rm rcp85_CNRM_CCLM_RR_daily_2???_v4.nc_month??.nc
+rm rcp85_CNRM_RCA_RR_daily_2???_v4.nc_month??.nc
 
 # Split into daily files
-for file in rcp85_CNRM_CCLM_RR_daily_2011-2100_??.nc
+for file in rcp85_CNRM_RCA_RR_daily_2011-2100_??.nc
 do cdo splitday $file ${file}_day
 done
 
 # Remove merged monthly files
-rm rcp85_CNRM_CCLM_RR_daily_2011-2100_??.nc
+rm rcp85_CNRM_RCA_RR_daily_2011-2100_??.nc
 
 # Rename files
-rename "s/.nc_day//g" rcp85_CNRM_CCLM_RR_daily_2011-2100_??.nc_day??.nc
+rename "s/.nc_day//g" rcp85_CNRM_RCA_RR_daily_2011-2100_??.nc_day??.nc
 
 # Copy current and future climate files (to be adjusted by the 3DBC R-scripts)
 module load nco #load newest nco version
 
 cd /lustre/storeB/users/andreasd/postclim/3DBC/pr/Cur/
-for file in hist_CNRM_CCLM_RR_daily_1971-2010_????.nc
+for file in hist_CNRM_RCA_RR_daily_1971-2010_????.nc
 do ncap2 -s 'precipitation__map_hist_daily=double(precipitation__map_hist_daily)*0.1' $file ../CurC/$file
 done
 
 cd /lustre/storeB/users/andreasd/postclim/3DBC/pr/Fut/
-for file in rcp85_CNRM_CCLM_RR_daily_2011-2100_????.nc
+for file in rcp85_CNRM_RCA_RR_daily_2011-2100_????.nc
 do ncap2 -s 'precipitation__map_rcp85_daily=double(precipitation__map_rcp85_daily)*0.1' $file ../FutC/$file
 done
 
@@ -154,37 +154,37 @@ done
 ## Temperature
 ###################
 ### Split yearly files into monthly files
-cd /lustre/storeB/project/KSS/proj/scenariodata/Temperature/netcdf/CNRM_CCLM/rcp85/
-for file in rcp85_CNRM_CCLM_TM_daily_2???_v4.nc
+cd /lustre/storeB/project/KSS/proj/scenariodata/Temperature/netcdf/CNRM_RCA/rcp85/
+for file in rcp85_CNRM_RCA_TM_daily_2???_v4.nc
 do cdo splitmon $file /lustre/storeB/users/andreasd/postclim/3DBC/tas/Fut/${file}_month
 done
 
 # Merge monthly files over all years
 cd /lustre/storeB/users/andreasd/postclim/3DBC/tas/Fut/
 for m in 01 02 03 04 05 06 07 08 09 10 11 12
-do cdo -r mergetime rcp85_CNRM_CCLM_TM_daily_2???_v4.nc_month${m}.nc rcp85_CNRM_CCLM_TM_daily_2011-2100_${m}.nc
+do cdo -r mergetime rcp85_CNRM_RCA_TM_daily_2???_v4.nc_month${m}.nc rcp85_CNRM_RCA_TM_daily_2011-2100_${m}.nc
 done
 
 # Remove single monthly files
-rm rcp85_CNRM_CCLM_TM_daily_2???_v4.nc_month??.nc
+rm rcp85_CNRM_RCA_TM_daily_2???_v4.nc_month??.nc
 
 # Split into daily files
-for file in rcp85_CNRM_CCLM_TM_daily_2011-2100_??.nc
+for file in rcp85_CNRM_RCA_TM_daily_2011-2100_??.nc
 do cdo splitday $file ${file}_day
 done
 
 # Remove merged monthly files
-rm rcp85_CNRM_CCLM_TM_daily_2011-2100_??.nc
+rm rcp85_CNRM_RCA_TM_daily_2011-2100_??.nc
 
 # Rename files
-rename "s/.nc_day//g" rcp85_CNRM_CCLM_TM_daily_2011-2100_??.nc_day??.nc
+rename "s/.nc_day//g" rcp85_CNRM_RCA_TM_daily_2011-2100_??.nc_day??.nc
 
 # Copy current and future climate files (to be adjusted by the 3DBC R-scripts)
 cd /lustre/storeB/users/andreasd/postclim/3DBC/tas/CurC/
-cp /lustre/storeB/users/andreasd/postclim/3DBC/tas/Cur/hist_CNRM_CCLM_TM_daily_1971-2010_????.nc .
+cp /lustre/storeB/users/andreasd/postclim/3DBC/tas/Cur/hist_CNRM_RCA_TM_daily_1971-2010_????.nc .
 
 cd /lustre/storeB/users/andreasd/postclim/3DBC/tas/FutC/
-cp /lustre/storeB/users/andreasd/postclim/3DBC/tas/Fut/rcp85_CNRM_CCLM_TM_daily_2011-2100_????.nc .
+cp /lustre/storeB/users/andreasd/postclim/3DBC/tas/Fut/rcp85_CNRM_RCA_TM_daily_2011-2100_????.nc .
 
 
 
